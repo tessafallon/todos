3b47b1a43a09394799680fa927c99c6a39e0728e
contessa <tessa.fallon@gmail.com>
2013-11-15 09:52:45 -0500

    fix folder

--- loops/fizzbuzz/fizzbuzz.rb
+++ loops/fizzbuzz/fizzbuzz.rb
@@ -1,15 +1,15 @@
 fizzbuzz = (1..100).each do |num|
-	if num%3 == 0 
-		puts "fizz"
+	if num % 15 == 0 
+			puts "FizzBuzz"
+
+	elsif num % 3 == 0 
+		puts "Fizz"
 	elsif
-		if num%5 == 0 
-			puts "buzz"
-	elsif
-		if num%5 == 0 && num%3 == 0 
-			puts "fizzbuzz"
+		num % 5 == 0 
+			puts "Buzz"
+	
 	else
 		puts num
 end
 end
-end
-end
+
