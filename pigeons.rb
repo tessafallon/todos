d982cb424ada2b07439f7dab941a4093c76ec4b3
contessa <tessa.fallon@gmail.com>
2013-11-18 14:55:00 -0500

    Add new pigeons.rb

---
+++ pigeons.rb
@@ -0,0 +1,86 @@
+########################
+# NYC PIGEON ORGANIZER #
+########################
+
+# Start with the following collected data on NYC pigeons.
+
+pigeon_data = { #hash
+  :color => { #hash
+    :purple => ["Theo", "Peter Jr.", "Lucky"], #array
+    :grey => ["Theo", "Peter Jr.", "Ms .K"],
+    :white => ["Queenie", "Andrew", "Ms .K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms .K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms .K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+
+
+pigeon_list1=[]
+pigeon_data[:color].each do |key, values|
+ pigeon_list1.concat(values)
+end
+new_list = pigeon_list1.sort.uniq!
+
+pigeon_list = Hash[new_list.map {|key, value| [key, value]}]
+puts "pigeon_list: #{pigeon_list.inspect}"
+
+def change_info
+if :purple includes NAME in values,
+  push :PURPLE to the VALUE of the NAME COLOR HASH.
+
+
+
+
+
+
+# Iterate over the hash above collecting each pigeon by name and insert it
+# as the key of a new hash where each name holds the attributes for that bird. 
+# Your output should match the hash below:
+
+# pigeon_list = {
+#   "Theo" => {
+#     :color => ["purple", "grey"],
+#     :gender => "male",
+#     :lives => "Subway"
+#   },
+#   "Peter Jr." => {
+#     :color => ["purple", "grey"],
+#     :gender => "male",
+#     :lives => "Library"
+#   },
+#   "Lucky" => {
+#     :color => ["purple"],
+#     :gender => "male",
+#     :lives => "City Hall"
+#   },
+#   "Ms .K" => {
+#     :color => ["grey", "white"],
+#     :gender => "female",
+#     :lives => "Central Park"
+#   },
+#   "Queenie" => {
+#     :color => ["white", "brown"],
+#     :gender => "female",
+#     :lives => "Subway"
+#   },
+#   "Andrew" => {
+#     :color => ["white"],
+#     :gender => "male",
+#     :lives => "Central Park"
+#   },
+#   "Alex" => {
+#     :color => ["white", "brown"],
+#     :gender => "male",
+#     :lives => "Central Park"
+#   }
+# }
