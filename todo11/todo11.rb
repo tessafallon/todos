8ad51d770e780cf38745b4f37ba10237ddc1d9a8
contessa <tessa.fallon@gmail.com>
2013-11-26 10:01:03 -0500

    Add working todo11

--- todo11/todo11.rb
+++ todo11/todo11.rb
@@ -1,10 +1,10 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-num_hash = {:num1=> 3, :num2=>45, :num3 => 17, :num4 => 5}
+num_hash = {:num1=> 33, :num2=>45, :num3 => 17, :num4 => 5}
 
 def key_for_min_value(hash)
-	min_v = hash.min {|val| val puts hash.min }
-	hash.each_pair {|k, v| (v==min_v) ? (puts k) : nil}
+	val = hash.min
+	hash.each {|hash| (hash.empty?) ? (puts nil) : (puts val[0])}
 end
 
 key_for_min_value(num_hash)
