50d0e69e6fd416008c95554a6d3cc7390a9c3724
contessa <tessa.fallon@gmail.com>
2013-11-26 10:49:09 -0500

    fix return values

--- todo11/todo11.rb
+++ todo11/todo11.rb
@@ -1,10 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-num_hash = {:num1=> 33, :num2=>45, :num3 => 17, :num4 => 5}
+num_hash = {:num1=> 33, :num2=> 45, :num3 => 17, :num4 => 5}
 
 def key_for_min_value(hash)
-	x =  hash.each_value.min 
-	hash.each_pair {|k,v| (v==x) ? (puts k) : nil }
+	values = []
+	hash.each {|k, v| values << v}
+	x = values.min
+	hash.each_pair {|k, v| (v==x) ? (return k) : nil}
 end
 
 key_for_min_value(num_hash)
