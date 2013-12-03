c4d5e94629b912bae978f688cbf8d2933a35691e
contessa <tessa.fallon@gmail.com>
2013-12-03 08:57:38 -0500

    Initial commit

---
+++ todo13/todo13.rb
@@ -0,0 +1,32 @@
+class Array
+  def version_sort
+      self.each {|name| puts name.match(/\d/)}
+  end
+end
+filenames = [
+  "foo-1.10.2.ext",
+  "foo-1.11.ext",
+  "foo-1.3.ext",
+  "foo-1.50.ext",
+  "foo-1.8.7.ext",
+  "foo-1.9.3.ext",
+  "foo-1.ext",
+  "foo-10.1.ext",
+  "foo-10.ext",
+  "foo-100.ext",
+  "foo-13.ext",
+  "foo-2.0.0.ext",
+  "foo-2.0.1.ext",
+  "foo-2.0.ext",
+  "foo-2.007.ext",
+  "foo-2.01.ext",
+  "foo-2.012b.ext",
+  "foo-2.01a.ext",
+  "foo-2.0a.ext",
+  "foo-2.0b.ext",
+  "foo-2.1.ext",
+  "foo-25.ext",
+  "foo-6.ext",
+]
+
+filenames.version_sort
