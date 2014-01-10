cb1f0ef79be5a4db09dca691cfb34b9def1dce00
contessa <tessa.fallon@gmail.com>
2014-01-10 09:16:15 -0500

    updated todo32

--- todo32/todo32.rb
+++ todo32/todo32.rb
@@ -3,12 +3,12 @@
 #Code:
 class Hash
   def keys_of(*args)
-    p self.key(*args)
+    self.keys do |key, value| if key(value) == key(args)
+    	return key
+    end
   end
 end
-h = Hash.new
-h = {a: 1, b: 2, c: 3, d: 1}
-h.keys_of(1)
+
 {a: 1, b: 2, c: 3}.keys_of(1) #=> [:a]
 {a: 1, b: 2, c: 3, d: 1}.keys_of(1) #=>  [:a, :d]
 {a: 1, b: 2, c: 3, d: 1}.keys_of(1, 2) #=> [:a, :b, :d]
