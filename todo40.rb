527629d58cac3228bc0ae7126a5aed495c9e0977
contessa <tessa.fallon@gmail.com>
2014-01-20 09:26:53 -0500

    todo40

---
+++ todo40.rb
@@ -0,0 +1,9 @@
+#Separate numbers with a comma each three digits.
+ 
+def separate_with_comma(n)
+	self.each do |n| if n.length > 2 
+end
+ 
+separate_with_comma(1) #=> "1" 
+separate_with_comma(1000) #=> "1,000" 
+separate_with_comma(10000) #=> "10,000"
